a4c7645eeeba9efed03052a648337e57b9b6281e
Rohit <rohit_macmini@Rohit-MacMinis-Mac-mini.local>
2019-01-21 18:37:05 +0530

    Login with twitter in react native.

---
+++ SocialLoginDemoProject/App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,NativeModules} from 'react-native';
+const { RNTwitterSignIn } = NativeModules
+
+const Constants = {
+  //Dev Parse keys
+  TWITTER_COMSUMER_KEY: "<Consumer API key>",
+  TWITTER_CONSUMER_SECRET: "<Consumer secret key>"
+}
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  _twitterSignIn = () => {
+    RNTwitterSignIn.init(Constants.TWITTER_COMSUMER_KEY, Constants.TWITTER_CONSUMER_SECRET)
+    RNTwitterSignIn.logIn()
+      .then(loginData => {
+        console.log(loginData)
+        const { authToken, authTokenSecret,email  } = loginData
+        if (authToken && authTokenSecret) {
+          
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      }
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome} onPress={this._twitterSignIn}>Click here to Twitter login</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
